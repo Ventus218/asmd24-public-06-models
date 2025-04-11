1665a24f7d2a245ebbc049491a9ed2903ba4151e
mirko.viroli <mirko.viroli@unibo.it>
2025-04-11 10:54:11 +0200

    07 ready for lab

--- src/main/scala/u07/utils/MSet.scala
+++ src/main/scala/u07/utils/MSet.scala
@@ -1,5 +1,7 @@
 package u07.utils
 
+import scala.collection.immutable
+
 // A multiset datatype
 trait MSet[A] extends (A => Int):
   def union(m: MSet[A]): MSet[A]
@@ -11,6 +13,10 @@ trait MSet[A] extends (A => Int):
   def asList: List[A]
   def asMap: Map[A,Int]
   def iterator: Iterator[A]
+  def map[B](f: (A) => B): MSet[B]
+  def flatMap[B](f: (A) => MSet[B]): MSet[B]
+  def filter(f: (A) => Boolean): MSet[A]
+  def collect[B](f: PartialFunction[A, B]): MSet[B]
 
 // Functional-style helpers/implementation
 object MSet:
@@ -24,7 +30,7 @@ object MSet:
     def this(list: List[A]) =
       this(list.groupBy(a=>a).map{case (a,n) => (a, n.size)})
     override val asList =
-      asMap.toList.flatMap{case (a,n) => List.fill(n)(a)}
+      asMap.toList.flatMap{case (a,n) => immutable.List.fill(n)(a)}
 
     override def apply(v1: A) = asMap.getOrElse(v1,0)
     override def union(m: MSet[A]) = new MSetImpl[A](asList ++ m.asList)
@@ -35,4 +41,11 @@ object MSet:
     override def extract(m: MSet[A]) =
       Some(this diff m) filter (_.size == size - m.size)
     override def iterator = asMap.keysIterator
+    override def map[B](f: (A) => B) = new MSetImpl[B](asList.map(f))
+    override def flatMap[B](f: (A) => MSet[B]) =
+      new MSetImpl[B](asList.flatMap(x => f(x).asList))
+    override def filter(f: (A) => Boolean) =
+      new MSetImpl[A](asList.filter(f))
+    override def collect[B](f: PartialFunction[A, B]) =
+      new MSetImpl[B](asList.collect(f))
     override def toString = s"{${asList.mkString("|")}}"
